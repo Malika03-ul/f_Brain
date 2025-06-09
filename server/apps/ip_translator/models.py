ffc9684529cde54705dbfcfd5668b9c5e142cf93
Malika03-ul <ndjonguetiadeumalikalucresse@gmail.com>
2025-06-09 17:06:15 +0200

    création dossier et initialisation du projet react

--- server/apps/ip_translator/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class IPTranslation(models.Model):
-    ipv4 = models.GenericIPAddressField(protocol='IPv4')
-    ipv6 = models.GenericIPAddressField(protocol='IPv6')
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.ipv4} -> {self.ipv6}"
